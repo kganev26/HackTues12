a7d2a1d3a7f1a9908c93b7ce80b56f0d448b941d
kganev26 <kganev26@gmail.com>
2026-03-26 01:17:36 +0200

    CurrentArduino

--- Arduino/arduino/arduino.ino
+++ Arduino/arduino/arduino.ino
@@ -1,9 +1,15 @@
 #include <DHT.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <SPI.h>
 
 #define DHTPIN 2
 #define DHTTYPE DHT11
 #define WATER_PIN  D5
 
+const char* ssid = "Daniel's S24";
+const char* password = "BatManSavage1976";
+const char* serverURL = "http://192.168.248.104:5500/cardnum";  
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -12,6 +18,11 @@ void setup() {
   dht.begin();
   pinMode(WATER_PIN, INPUT);
   Serial.println("[Water Sensor] Ready.");
+  WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+    }
+
 }
 
 void loop() {
@@ -22,7 +33,6 @@ void loop() {
     Serial.println("Failed to read from DHT!");
     return;
   }
-
   Serial.print("Temp: ");
   Serial.print(temp);
   Serial.print(" °C  |  Humidity: ");
@@ -31,7 +41,10 @@ void loop() {
   
   bool waterDetected = (digitalRead(WATER_PIN) == HIGH);
   printWater(waterDetected);
-  
+  String uidString = "";
+  byte i = 1
+  uid
+  sendUIDToServer(uidString);
   delay(2000);
 }
 
@@ -40,4 +53,24 @@ void printWater(bool detected) {
   Serial.println("┌──────────────────────────┐");
   Serial.printf( "│ Water: %-18s│\n", detected ? "DETECTED ⚠" : "DRY");
   Serial.println("└──────────────────────────┘");
+}
+void sendUIDToServer(String uid) {
+    if (WiFi.status() == WL_CONNECTED) {
+        WiFiClient client;
+        HTTPClient http;
+        http.begin(client, serverURL);
+        http.addHeader("Content-Type", "application/json");
+        String jsonPayload = "{\"uid\":\"" + uid + "\"}";
+        int httpResponseCode = http.POST(jsonPayload);
+        Serial.print(jsonPayload);
+        if (httpResponseCode > 0) {
+            String response = http.getString();
+            Serial.println(response);
+        } else {
+            Serial.print("Error");
+        }
+        http.end();
+    } else {
+        WiFi.begin(ssid, password);
+    }
 }
