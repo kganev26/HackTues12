1627c32785ba1a3383ffd49e457ac16f57a97b22
kganev26 <kganev26@gmail.com>
2026-03-26 00:08:01 +0200

    DHT11ANDWaterSensor

--- Arduino/arduino/arduino.ino
+++ Arduino/arduino/arduino.ino
@@ -1,1 +1,43 @@
-#include <ESP8266WiFi.h>
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+#define WATER_PIN  D5
+
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(WATER_PIN, INPUT);
+  Serial.println("[Water Sensor] Ready.");
+}
+
+void loop() {
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  if (isnan(temp) || isnan(hum)) {
+    Serial.println("Failed to read from DHT!");
+    return;
+  }
+
+  Serial.print("Temp: ");
+  Serial.print(temp);
+  Serial.print(" °C  |  Humidity: ");
+  Serial.print(hum);
+  Serial.println(" %");
+  
+  bool waterDetected = (digitalRead(WATER_PIN) == HIGH);
+  printWater(waterDetected);
+  
+  delay(2000);
+}
+
+
+void printWater(bool detected) {
+  Serial.println("┌──────────────────────────┐");
+  Serial.printf( "│ Water: %-18s│\n", detected ? "DETECTED ⚠" : "DRY");
+  Serial.println("└──────────────────────────┘");
+}
