ce8c745c3af734fa39057f6f392a6acd95c76fc0
Bobarom <bobarkumanov@gmail.com>
2026-03-26 16:46:54 +0200

    centrelized the ip

--- react gaia/next.config.mjs
+++ react gaia/next.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
+const FLASK_URL = process.env.FLASK_URL || "http://localhost:5500"
+
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${FLASK_URL}/:path*`,
+      },
+    ]
   },
 }
 
